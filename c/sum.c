73f6ef904348d0b5b85988bc34be1c3299e4fa59
Lena <evassanna@yandex.ru>
2019-03-17 00:44:10 +0300

    first try, functions

---
+++ c/sum.c
@@ -0,0 +1,13 @@
+#include "stdio.h" 
+int sum(int a, int b)
+{
+ return a+b;
+}
+
+int main(void)
+{
+       int result = sum(15,3);
+       printf("Сумма - %d", result);
+       return 0;
+
+}
---
+++ js/sum.js
@@ -0,0 +1,7 @@
+function sum(){
+    a=15;
+    b=25;
+    result = a+b;
+    console.log(result);
+}
+sum;
